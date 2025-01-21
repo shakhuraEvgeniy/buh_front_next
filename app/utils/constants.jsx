8949b8bab9a603a228ba7a62fbe155539063fe4b
shah <shah5@yandex.ru>
2025-01-21 22:36:25 +0300

    add server

--- app/utils/constants.jsx
+++ app/utils/constants.jsx
@@ -1,2 +1,2 @@
-export const MAIN_URL = "http://192.168.1.99:3000";
-// export const MAIN_URL = "http://127.0.0.1:3001";
+// export const MAIN_URL = "http://192.168.1.99:3000";
+export const MAIN_URL = "http://127.0.0.1:3000";
