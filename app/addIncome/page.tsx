6ba1d5ba927acfe8bf3e855e90c36d3438564621
Шахура Евгений <shahura.evgeniy@wb.ru>
2025-01-21 23:05:51 +0300

    add income item

--- app/addIncome/page.tsx
+++ app/addIncome/page.tsx
@@ -1,17 +1,19 @@
-"use client"
-import * as categoryApi from "@/app/utils/api/categorys";
-import * as costsApi from "@/app/utils/api/costs";
-import * as accountsApi from "@/app/utils/api/accounts";
-import { Account, Category } from "@/app/lib/definitions";
+'use client';
 import { useEffect, useState } from 'react';
-import { useFormWithValidation } from "@/app/hooks/useFormWithValidation";
 import FormAddCostAndIncome from '../ui/form/form';
+import { Account, Category } from '../lib/definitions';
+import * as accountsApi from "@/app/utils/api/accounts";
+import * as incomesApi from "@/app/utils/api/incomes";
+import * as categoryApi from "@/app/utils/api/categorys";
+import { useFormWithValidation } from '../hooks/useFormWithValidation';
 import { getCurrentDateTime } from '../utils/getDate';
 
-export default function AddCosts() {
+
+
+export default function AddIncome() {
+  const [accounts, setAccounts] = useState<Account[]>([]);
   const [categorys, setCategorys] = useState<Category[]>([]);
   const [subCategorys, setSubCategorys] = useState<Category[]>([]);
-  const [accounts, setAccounts] = useState<Account[]>([]);
 
   const { values, handleChange, resetForm } = useFormWithValidation({
     sum: 0,
@@ -23,36 +25,17 @@ export default function AddCosts() {
   });
 
   useEffect(() => {
-    getCategiryCost()
+    getCategiryIncome()
     getAccounts()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     if (values.categoryId) {
-      getSubCategorysCost(values.categoryId);
+      getSubCategorysIncome(values.categoryId);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [values.categoryId]);
-
-  const getCategiryCost = async () => {
-    try {
-      const cat = await categoryApi.getCategorysCostApi();
-      setCategorys(cat);
-    } catch (e) {
-      console.log(e);
-    };
-  }
-
-  const getSubCategorysCost = async (id: number) => {
-    try {
-      const subCat = await categoryApi.getSubCategorysCostApi(id);
-      setSubCategorys(subCat);
-      values.subCategoryId = 0
-    } catch (e) {
-      console.log(e);
-    };
-  }
 
   const getAccounts = async () => {
     try {
@@ -64,12 +47,40 @@ export default function AddCosts() {
     };
   }
 
+  const getCategiryIncome = async () => {
+    try {
+      const cat = await categoryApi.getCategorysIncomeApi();
+      setCategorys(cat);
+    } catch (e) {
+      console.log(e);
+    };
+  }
+
+  const getSubCategorysIncome = async (id: number) => {
+    try {
+      const subCat = await categoryApi.getSubCategorysIncomeApi(id);
+      setSubCategorys(subCat);
+      values.subCategoryId = 0
+    } catch (e) {
+      console.log(e);
+    };
+  }
+
+  const handleChangeCategory = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    try {
+      handleChange(e);
+      await getSubCategorysIncome(Number(e.target.value));
+    } catch (e) {
+      console.log(e);
+    };
+  }
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       const createTimeWithTime = new Date(`${values.createTime}T${getCurrentDateTime().slice(11,)}`);
       const subCategoryId = values.subCategoryId === 0 ? null : Number(values.subCategoryId);
-      await costsApi.addCostsApi(Number(values.accountId), createTimeWithTime, Number(values.sum), Number(values.categoryId), subCategoryId, values.comment);
+      await incomesApi.addIncomeApi(Number(values.accountId), createTimeWithTime, Number(values.sum), Number(values.categoryId), subCategoryId, values.comment);
       resetForm({
         sum: 0,
         comment: "",
@@ -79,15 +90,6 @@ export default function AddCosts() {
         createTime: values.createTime,
       })
 
-    } catch (e) {
-      console.log(e);
-    };
-  }
-
-  const handleChangeCategory = async (e: React.ChangeEvent<HTMLSelectElement>) => {
-    try {
-      handleChange(e);
-      await getSubCategorysCost(Number(e.target.value));
     } catch (e) {
       console.log(e);
     };
